e6978fa3783a1b46bb04eb97e00106cf40e36f09
Milan Zivkovic <zivkovic.milan@gmail.com>
2019-05-23 21:55:21 +0200

    git hub pages setup

---
+++ docs/precache-manifest.cf73455f08c946ae79fae1fbd7896b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbbe37798bd9ead780001dcb9985582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "689970680d38b61abcc9",
+    "url": "/static/css/main.e33b24bf.chunk.css"
+  },
+  {
+    "revision": "0f44f52868a50c2f6362",
+    "url": "/static/js/2.d23e1248.chunk.js"
+  },
+  {
+    "revision": "689970680d38b61abcc9",
+    "url": "/static/js/main.574e7e0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
